643534dc6f73b33ed44d9d72e8fec18f5704c492
Danny <Danny007111@hotmail.com>
2022-03-17 13:50:51 -0500

    Working on Q1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+    Danny: "23rd street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const destructivelyUpdateObject = updateEmployeeWithKeyAndValue(employee, "Nayeli", "Washtenaw");
+};
+
+
+
+
